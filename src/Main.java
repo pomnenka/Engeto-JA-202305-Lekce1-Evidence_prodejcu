66c9aa3dce3332a6646ba817cbb35bec08b89919
Honza T <jan.trasak@email.cz>
2023-06-05 13:20:11 +0200

    initial commit

---
+++ src/Main.java
@@ -0,0 +1,19 @@
+import java.net.InetAddress;
+
+public class Main {
+    public static void main(String[] args) {
+
+        String firstName;
+        String familyName;
+        int numberOfContracts;
+        double carrotSold;
+        String addressCity;
+        String carRegistration;
+        double carFuelConsumption;
+        String customerComputerIPaddress;
+
+        customerComputerIPaddress = "136.97.34.214";
+
+        System.out.println("IP address: " + customerComputerIPaddress);
+    }
+}
